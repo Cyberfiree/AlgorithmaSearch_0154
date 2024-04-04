f6d8227713b4e66539ee47bc03fde35c8e0f0880
Cyberfire <buayagoreng99@gmail.com>
2024-04-04 11:56:15 +0800

    membuat Deklarasi array arr dengan panjang 20.

--- linearsearch/linearsearch.cpp
+++ linearsearch/linearsearch.cpp
@@ -1,4 +1,7 @@
-#include <iostream>
+#include <iostream> // Menyertakan pustaka iostream untuk input-output.
+using namespace std; // Menggunakan namespace std untuk menghindari penulisan std:: sebelum fungsi-fungsi standar.
+
+int arr[20]; //// Deklarasi array arr dengan panjang 20.
 
 int main()
 {

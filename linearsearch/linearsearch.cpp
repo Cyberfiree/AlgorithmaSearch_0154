425a728c14c4bd0a65302fa2c6a006ab02775f11
Cyberfire <buayagoreng99@gmail.com>
2024-04-04 11:57:09 +0800

    membuat variabel untuk menyimpan jumlah elemen array.

--- linearsearch/linearsearch.cpp
+++ linearsearch/linearsearch.cpp
@@ -1,7 +1,8 @@
 #include <iostream> // Menyertakan pustaka iostream untuk input-output.
 using namespace std; // Menggunakan namespace std untuk menghindari penulisan std:: sebelum fungsi-fungsi standar.
 
-int arr[20]; //// Deklarasi array arr dengan panjang 20.
+int arr[20]; // Deklarasi array arr dengan panjang 20.
+int n; // Variabel untuk menyimpan jumlah elemen array.
 
 int main()
 {

7374b19d972009bf7a6b74b6a1184abcd1f52895
Cyberfire <buayagoreng99@gmail.com>
2024-04-04 12:10:51 +0800

    membuat ariabel untuk menghitung jumlah perbandingan

--- linearsearch/linearsearch.cpp
+++ linearsearch/linearsearch.cpp
@@ -29,6 +29,7 @@ void input() // Fungsi untuk mengambil input jumlah dan elemen array.
 void LinearSearch() //Fungsi untuk melakukan pencarian linear
 {
 	char ch; // Variabel untuk menyimpan pilihan pengguna
+	int ctr; // ariabel untuk menghitung jumlah perbandingan
 }
 int main()
 {

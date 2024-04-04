77d3a09936dd8e1ff2ec88c2722c99794d2b8cf7
Cyberfire <buayagoreng99@gmail.com>
2024-04-04 12:11:21 +0800

    membuat Variabel untuk menyimpan elemen yang akan dicari

--- linearsearch/linearsearch.cpp
+++ linearsearch/linearsearch.cpp
@@ -29,7 +29,8 @@ void input() // Fungsi untuk mengambil input jumlah dan elemen array.
 void LinearSearch() //Fungsi untuk melakukan pencarian linear
 {
 	char ch; // Variabel untuk menyimpan pilihan pengguna
-	int ctr; // ariabel untuk menghitung jumlah perbandingan
+	int ctr; // variabel untuk menghitung jumlah perbandingan
+	int item; // Variabel untuk menyimpan elemen yang akan dicari
 }
 int main()
 {

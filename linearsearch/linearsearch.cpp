fc3290f4d166936a47b08f8ba1a281dad914ac88
Cyberfire <buayagoreng99@gmail.com>
2024-04-04 12:00:43 +0800

    Mengambil input jumlah elemen.

--- linearsearch/linearsearch.cpp
+++ linearsearch/linearsearch.cpp
@@ -10,6 +10,7 @@ void input() // Fungsi untuk mengambil input jumlah dan elemen array.
 	while (true) //melakukan loop tak terbatas
 	{
 		cout << "Enter the number of elements in the array: "; // Meminta input jumlah elemen array.
+		cin >> n; // Mengambil input jumlah elemen.
 	}
 }
 int main()
